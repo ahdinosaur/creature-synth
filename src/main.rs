e21b675916adf13b77b40dc6bd6c531cfffdfb60
Michael Williams <hello@mikey.nz>
2025-11-17 09:00:08 +1300

    Add FPS overlay

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod creature;
 mod limb;
 mod oscillator;
 
-use bevy::prelude::*;
+use bevy::{dev_tools::fps_overlay::FpsOverlayPlugin, prelude::*};
 
 use crate::{
     creature::spawn_creature,
@@ -13,6 +13,9 @@ use crate::{
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(FpsOverlayPlugin {
+            ..Default::default()
+        })
         .add_systems(Startup, (setup_camera, spawn_creature))
         .add_systems(Update, oscillator_tick.before(animate_limb_segments))
         .add_systems(Update, oscillator_user_update)
